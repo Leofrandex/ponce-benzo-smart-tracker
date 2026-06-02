ec59dce423e234e2cd7bbfdfc4a0def8e98eb168
Sebastian Castro <sebastian.castro@CLMD.LOCAL>
2026-06-02 01:28:03 -0400

    feat(mapa): live view with stores and active merchandisers

---
+++ hub/app/components/mapa/MapLiveView.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useMemo } from "react";
+import { mockStores } from "@/app/lib/mock-data";
+import { mockActiveMerchandisers } from "@/app/lib/map-data";
+import { LIGHT_TILE_URL, LIGHT_TILE_ATTRIBUTION, CARACAS_CENTER } from "./tiles";
+import { StoreMarkersLayer } from "./StoreMarkersLayer";
+import { MerchandiserMarkersLayer } from "./MerchandiserMarkersLayer";
+import type { MapFilterValue } from "./MapFilters";
+
+export default function MapLiveView({ filters }: { filters: MapFilterValue }) {
+  const stores = useMemo(() => filters.storeIds.length === 0 ? mockStores : mockStores.filter((s) => filters.storeIds.includes(s.store_id)), [filters.storeIds]);
+  const merchandisers = useMemo(() => filters.merchIds.length === 0 ? mockActiveMerchandisers : mockActiveMerchandisers.filter((m) => filters.merchIds.includes(m.id)), [filters.merchIds]);
+
+  return (
+    <MapContainer center={CARACAS_CENTER} zoom={13} style={{ width: "100%", height: "100%", zIndex: 1 }} zoomControl={false}>
+      <TileLayer url={LIGHT_TILE_URL} attribution={LIGHT_TILE_ATTRIBUTION} />
+      <StoreMarkersLayer stores={stores} />
+      <MerchandiserMarkersLayer merchandisers={merchandisers} />
+    </MapContainer>
+  );
+}
